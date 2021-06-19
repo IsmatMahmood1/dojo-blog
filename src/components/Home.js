47fd987f9baf700aa61758c6f856c39567b90cf9
IsmMah <ismat.mahmood@softwire.com>
2021-06-19 22:07:23 +0100

    added Home Route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,20 @@
-import { useState, useEffect } from "react";
 import { BlogList } from "./BlogList";
+import { useFetch } from "./useFetch";
 
-export function Home () {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
+export function Home() {
+    const { data:blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
 
-
-    
-    
-    useEffect(()=> {
-        fetch ('http://localhost:8000/blogs')
-            .then(response => { 
-                return response.json ()})
-                .then((data) => {setIsPending(false); setBlogs(data)})
-
-    },[]);
+   
 
     return (
         <div className='home'>
-        { isPending && <div>Loading...</div>}
-         {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
+            { error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} />}
 
-       
-            
+
+
         </div>
     )
 }
